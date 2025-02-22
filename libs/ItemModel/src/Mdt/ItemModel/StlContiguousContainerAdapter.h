4b700314b7daa30edc7ef1b296cbec4f19e2759c
Philippe Steinmann <philippe.steinmann@gmail.com>
2025-02-22 08:47:07 +0100

    Work on STL container adapters
    Still a sandbox
    GL-1

--- libs/ItemModel/src/Mdt/ItemModel/StlContiguousContainerAdapter.h
+++ libs/ItemModel/src/Mdt/ItemModel/StlContiguousContainerAdapter.h
@@ -10,16 +10,198 @@
 #ifndef MDT_ITEM_MODEL_STL_CONTIGUOUS_CONTAINER_ADAPTER_H
 #define MDT_ITEM_MODEL_STL_CONTIGUOUS_CONTAINER_ADAPTER_H
 
+#include "Mdt/TypeTraits/StlContainerHelpers.h"
 #include <Mdt/Numeric/Limits.h>
 #include <Mdt/Numeric/BasicConversion.h>
+#include <type_traits>
+#include <utility>
 #include <cassert>
 
 namespace Mdt{ namespace ItemModel{
+
+  /*! \brief Adapter to use STL style containers with Qt item models
+   *
+   *
+   * This adapter can be used with containers that are domain specific,
+   * and that do not provide all the STL required interface.
+   *
+   * Goal is to avoid having to adapt the container itself
+   * to be usable with an item model.
+   *
+   * \todo fix noexcept
+   *
+   * \tparam Container
+   * \tparam SizeType
+   *
+   * \tparam ValueType STL value_type provided by the container.
+   * This is a required type, void is not allowed.
+   * const_reference is also deduced from ValueType.
+   *
+   * \tparam ConstIterator STL const_iterator provided by the container.
+   * Can be void if the container not provides const_iterator.
+   * If the container provides const_iterator,
+   * it also must provide cbegin() and cend().
+   *
+   * \tparam Reference STL reference provided by the container.
+   *
+   *
+   * \tparam FunctionMap
+   *
+   * \sa Mdt::ItemModel::AbstractTableModel
+   * \sa https://doc.qt.io/qt-6/qabstractitemmodel.html
+   * \sa https://doc.qt.io/qt-6/qmodelindex.html
+   */
 
   /*! \brief Adapter to use STL contiguous containers with Qt item models
    *
    * In Qt model/view, row acces is int index based.
    * In the STL containers, index is std::size_t or iterator based.
+   *
+   * A goal of this adapter is to help to convert between those types
+   * in some checked way (contract programming based).
+   *
+   * To be usable with domain specific containers,
+   * this adapter requires a minimal subset of the STL requirements.
+   *
+   * To avoid having to modify domain specific containers,
+   * a \a FunctionMap can be provided,
+   * to map the domain specific container's methods to this adapter.
+   *
+   * \section Mdt_ItemModel_StlContiguousContainerAdapter_MinimalReadOnlyContainer A minimal read only container example
+   *
+   * Example of a simple read only container:
+   * \code
+   * class ReadOnlyList
+   * {
+   *  public:
+   *
+   *   using size_type = std::vector<Item>::size_type;
+   *
+   *   size_type getSizeCustom() const noexcept;
+   *   const Item & itemAt(size_type index) const noexcept;
+   * };
+   * \endcode
+   *
+   * This domain container exposes \a size_type in a STL standard way.
+   * It also provides domain specific methods.
+   * To be usable with this adapter,
+   * we have to provide a function map:
+   * \code
+   * struct ReadOnlyListTableModelAdapterFunctionMap
+   * {
+   *   using size_type = ReadOnlyList::size_type;
+   *   using const_reference = const Item &;
+   *
+   *   static
+   *   size_type size(const ReadOnlyList & list) noexcept
+   *   {
+   *     return list.getSizeCustom();
+   *   }
+   *
+   *   static
+   *   const_reference atIndex(const ReadOnlyList & list, size_type index) noexcept
+   *   {
+   *     return list.itemAt(index);
+   *   }
+   * };
+   * \endcode
+   *
+   * A function map provides a set of STL types, and also static methods.
+   * The first argument of those methods is the container.
+   * This choice was made to avoid referencing the container in the function map,
+   * to prevent life time issues.
+   *
+   * Now we can implement the table model:
+   * \code
+   * class ReadOnlyListTableModel : QAbstractTableModel
+   * {
+   *  public:
+   *
+   *   ReadOnlyListTableModel(const ReadOnlyList & list, QObject *parent = nullptr)
+   *    : QAbstractTableModel(parent),
+   *      mList(list)
+   *   {
+   *   }
+   *
+   *   int rowCount( const QModelIndex &parent = QModelIndex() ) const override
+   *   {
+   *     // parent checking omitted here
+   *     return mList.rowCount();
+   *   }
+   *
+   *   int columnCount( const QModelIndex &parent = QModelIndex() ) const override
+   *   {
+   *     // parent checking omitted here
+   *     return 2;
+   *   }
+   *
+   *   QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override
+   *   {
+   *     // index + role checking omitted here
+   *     switch( index.column() ){
+   *       case 0:
+   *         return mList.atRow( index.row() ).id();
+   *       case 1:
+   *         return mList.atRow( index.row() ).name();
+   *     }
+   *
+   *     return QVariant();
+   *   }
+   *
+   *  private:
+   *
+   *   Mdt::ItemModel::StlContiguousContainerAdapter<ReadOnlyList, ReadOnlyListTableModelAdapterFunctionMap> mList;
+   * };
+   * \endcode
+   *
+   * We could also implement our model using Mdt::ItemModel::AbstractTableModel:
+   * \code
+   * class ReadOnlyListTableModel : public Mdt::ItemModel::AbstractTableModel
+   * {
+   *  public:
+   *
+   *   ReadOnlyListTableModel(const ReadOnlyList & list, QObject *parent = nullptr)
+   *    : AbstractTableModel(parent),
+   *      mList(list)
+   *   {
+   *   }
+   *
+   *  private:
+   *
+   *   int rowCountWithoutParentIndex() const noexcept override
+   *   {
+   *     return mList.rowCount();
+   *   }
+   *
+   *   int columnCountWithoutParentIndex() const noexcept override
+   *   {
+   *     return 2;
+   *   }
+   *
+   *   QVariant displayRoleData(const QModelIndex & index) const noexcept override
+   *   {
+   *     assert( indexIsValidAndInRange(index) );
+   *
+   *     switch( index.column() ){
+   *       case 0:
+   *         return mList.atRow( index.row() ).id();
+   *       case 1:
+   *         return mList.atRow( index.row() ).name();
+   *     }
+   *
+   *     return QVariant();
+   *   }
+   *
+   *   Mdt::ItemModel::StlContiguousContainerAdapter<ReadOnlyList, ReadOnlyListTableModelAdapterFunctionMap> mList;
+   * };
+   * \endcode
+   *
+   * \todo Document the following examples with AbstractTableModel
+   *
+   * \todo document minimal requirements on the container.
+   * For types, size_type and const_reference .
+   *
+   * \todo example with a container that provides all requirements, like std::vector, or one with less than vector, but all for this adapter
    *
    * \todo Discuss default constructed:
    * - Should it exist in adapter ? Yes
@@ -50,7 +232,7 @@ namespace Mdt{ namespace ItemModel{
    *
    * \todo Put usage example
    *
-   * \section Mdt_ItemModel_StlContiguousContainerAdapter_ReadOnlyContainer Read only container
+   * \section Mdt_ItemModel_StlContiguousContainerAdapter_MoreRealReadOnlyContainer A more real read only container example
    *
    * Imagine we implement a serial port settings editor.
    * One of the settings is the interface.
@@ -136,7 +318,7 @@ namespace Mdt{ namespace ItemModel{
    * {
    *  public:
    *
-   *   // Constructor and othet methods omitted here
+   *   // Constructor and other methods omitted here
    *
    *   int rowCount( const QModelIndex &parent = QModelIndex() ) const
    *   {
@@ -278,28 +460,77 @@ namespace Mdt{ namespace ItemModel{
    * \sa https://doc.qt.io/qt-6/qabstractitemmodel.html
    * \sa https://doc.qt.io/qt-6/qmodelindex.html
    * \sa https://en.cppreference.com/w/cpp/named_req/ContiguousContainer
+   *
+   * \section Mdt_ItemModel_StlContiguousContainerAdapter_Rationale Rationale
+   *
    */
-  template<typename Container>
+  template<typename Container, typename FunctionMap>
   class StlContiguousContainerAdapter
   {
    public:
 
-    using size_type = typename Container::size_type;
-    using const_reference = typename Container::const_reference;
+    /*! \brief STL size_type
+     *
+     * Will be FunctionMap::size_type
+     *
+     * size_type is mandatory
+     */
+    using size_type = typename FunctionMap::size_type;
 
-    /*! \brief Construct an empty container
+    /*! \brief STL const_reference
+     *
+     * Will be FunctionMap::const_reference
+     *
+     * const_reference is mandatory
+     */
+    using const_reference = typename FunctionMap::const_reference;
+
+    /*! \brief Construct an adapter with a default constructed container
      */
     explicit
-    StlContiguousContainerAdapter() noexcept = default;
+    StlContiguousContainerAdapter() noexcept(std::is_nothrow_default_constructible_v<Container>) = default;
+
+    // // explicit
+    // StlContiguousContainerAdapter(Container && container)
+    //  : mContainer( std::forward(container) )
+    // {
+    // }
+
+    /*! \brief Construct an adapter with a copy of given container
+     */
+    explicit
+    StlContiguousContainerAdapter(const Container & container) noexcept(std::is_nothrow_copy_constructible_v<Container>)
+     : mContainer(container)
+    {
+    }
+
+    /*! \brief Construct an adapter with given container moved in
+     */
+    explicit
+    StlContiguousContainerAdapter(Container && container) noexcept(std::is_nothrow_move_constructible_v<Container>)
+     : mContainer( std::move(container) )
+    {
+    }
+
+    /*! \brief Get the count of rows for the model
+     *
+     * \pre The current size of the container must be convertible to int
+     */
+    int rowCount() const
+    {
+      assert( Mdt::Numeric::int_canHoldValueOf_T( FunctionMap::size(mContainer) ) );
+
+      return Mdt::Numeric::int_from_T( FunctionMap::size(mContainer) );
+    }
 
     /*! \brief Get the count of elements
      */
-    int size() const noexcept
-    {
-      assert( Mdt::Numeric::int_canHoldValueOf_size_t( mContainer.size() ) );
-
-      return Mdt::Numeric::int_from_size_t( mContainer.size() );
-    }
+    // int size() const noexcept
+    // {
+    //   assert( Mdt::Numeric::int_canHoldValueOf_size_t( mContainer.size() ) );
+    //
+    //   return Mdt::Numeric::int_from_size_t( mContainer.size() );
+    // }
 
     // bool isEmpty()
 

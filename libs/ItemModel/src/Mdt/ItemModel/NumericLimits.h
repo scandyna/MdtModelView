3d4f15d7f02c4a9b1862a43abe561b4f18d10cce
Philippe Steinmann <philippe.steinmann@gmail.com>
2025-03-17 10:19:27 +0100

    Numeric limits: implement minBetweenSizeTypeValueAndIntMax() and minBetweenSizeTypeMaxAndIntMax()
    GL-1

--- libs/ItemModel/src/Mdt/ItemModel/NumericLimits.h
+++ libs/ItemModel/src/Mdt/ItemModel/NumericLimits.h
@@ -4,13 +4,18 @@
  ** MdtModelView
  ** Set of libraries extending the Qt model-view framework.
  **
- ** Copyright (C) 2023-2023 Philippe Steinmann.
+ ** Copyright (C) 2023-2025 Philippe Steinmann.
  **
  *****************************************************************************************/
 #ifndef MDT_ITEM_MODEL_NUMERIC_LIMITS_H
 #define MDT_ITEM_MODEL_NUMERIC_LIMITS_H
 
+#include <Mdt/Numeric/Limits.h>
+#include <Mdt/Numeric/BasicConversion.h>
+#include <Mdt/Numeric/IntegerComparison.h>
 #include <limits>
+#include <type_traits>
+#include <cassert>
 
 namespace Mdt{ namespace ItemModel{
 
@@ -34,6 +39,43 @@ namespace Mdt{ namespace ItemModel{
   bool isIntMax(int i) noexcept
   {
     return i == intMax();
+  }
+
+  /*! \brief Returns the minimum value between given \a value and int max
+   *
+   * \pre \a value must be an integral type
+   */
+  template<typename SizeType>
+  constexpr
+  int minBetweenSizeTypeValueAndIntMax(SizeType value) noexcept
+  {
+    static_assert(std::is_integral_v<SizeType>, "Mdt::ItemModel::minBetweenSizeTypeValueAndIntMax(): given value must be an integral type");
+    static_assert(!std::is_same_v<SizeType, bool>, "Mdt::ItemModel::minBetweenSizeTypeValueAndIntMax(): given value must be an integral type (not bool)");
+    static_assert(!std::is_same_v<SizeType, char>, "Mdt::ItemModel::minBetweenSizeTypeValueAndIntMax(): given value must be an integral type (not char)");
+
+    if( Mdt::Numeric::cmp_less( value, intMax() ) ){
+      if( Mdt::Numeric::int_canHoldValueOf_T(value) ){
+        return Mdt::Numeric::int_from_T(value);
+      }
+      return std::numeric_limits<int>::min();
+    }
+
+    return intMax();
+  }
+
+  /*! \brief Returns the minimum value between \a SizeType max and int max
+   *
+   * \pre \a SizeType must be an integral type
+   */
+  template<typename SizeType>
+  constexpr
+  int minBetweenSizeTypeMaxAndIntMax() noexcept
+  {
+    static_assert(std::is_integral_v<SizeType>, "Mdt::ItemModel::minBetweenSizeTypeMaxAndIntMax(): SizeType must be an integral type");
+    static_assert(!std::is_same_v<SizeType, bool>, "Mdt::ItemModel::minBetweenSizeTypeMaxAndIntMax(): SizeType must be an integral type (not bool)");
+    static_assert(!std::is_same_v<SizeType, char>, "Mdt::ItemModel::minBetweenSizeTypeMaxAndIntMax(): SizeType must be an integral type (not char)");
+
+    return minBetweenSizeTypeValueAndIntMax( std::numeric_limits<SizeType>::max() );
   }
 
 }} // namespace Mdt{ namespace ItemModel{
--- libs/ItemModel/tests/src/NumericLimitsTest.cpp
+++ libs/ItemModel/tests/src/NumericLimitsTest.cpp
@@ -4,15 +4,24 @@
  ** MdtModelView
  ** Set of libraries extending the Qt model-view framework.
  **
- ** Copyright (C) 2023-2023 Philippe Steinmann.
+ ** Copyright (C) 2023-2025 Philippe Steinmann.
  **
  *****************************************************************************************/
 #include "catch2/catch.hpp"
 #include "Catch2QString.h"
 #include "Mdt/ItemModel/NumericLimits.h"
+#include <cstddef>
+#include <cstdint>
+#include <limits>
 
 using namespace Mdt::ItemModel;
 
+/*
+ * See:
+ * - https://en.cppreference.com/w/cpp/language/types
+ * - https://en.cppreference.com/w/cpp/types/numeric_limits
+ * - https://en.cppreference.com/w/cpp/types/integer
+ */
 
 TEST_CASE("isIntMax")
 {
@@ -24,5 +33,201 @@ TEST_CASE("isIntMax")
   SECTION("int max")
   {
     REQUIRE( isIntMax( intMax() ) );
+  }
+}
+
+TEST_CASE("minBetweenSizeTypeValueAndIntMax_size_t")
+{
+  SECTION("0 is less than int max")
+  {
+    const std::size_t v = 0;
+
+    CHECK( minBetweenSizeTypeValueAndIntMax(v) == 0 );
+  }
+
+  SECTION("25 is less than int max")
+  {
+    const std::size_t v = 25;
+
+    CHECK( minBetweenSizeTypeValueAndIntMax(v) == 25 );
+  }
+
+  SECTION("int max")
+  {
+    const std::size_t v = intMax();
+
+    CHECK( minBetweenSizeTypeValueAndIntMax(v) == intMax() );
+  }
+
+  SECTION("int max is less than size_t max")
+  {
+    const std::size_t v = std::numeric_limits<std::size_t>::max();
+
+    CHECK( minBetweenSizeTypeValueAndIntMax(v) == intMax() );
+  }
+}
+
+TEST_CASE("minBetweenSizeTypeValueAndIntMax_unsigned_short_int")
+{
+  SECTION("0 is less than int max")
+  {
+    const unsigned short int v = 0;
+
+    CHECK( minBetweenSizeTypeValueAndIntMax(v) == 0 );
+  }
+
+  SECTION("25 is less than int max")
+  {
+    const unsigned short int v = 25;
+
+    CHECK( minBetweenSizeTypeValueAndIntMax(v) == 25 );
+  }
+
+  if constexpr(sizeof(int) == 4){
+    /*
+     * unsigned short int: 0 to 65535
+     * (signed) int: −2,147,483,648 to 2,147,483,647
+     */
+    SECTION("unsigned short int max is less than int max")
+    {
+      const unsigned short int v = std::numeric_limits<unsigned short int>::max();
+
+      CHECK( minBetweenSizeTypeValueAndIntMax(v) == v );
+    }
+  }else if constexpr(sizeof(int) == 2){
+    /*
+     * unsigned short int: 0 to 65535
+     * (signed) int:  −32768 to 32767
+     */
+    SECTION("unsigned short int max is greater than int max")
+    {
+      const unsigned short int v = std::numeric_limits<unsigned short int>::max();
+
+      CHECK( minBetweenSizeTypeValueAndIntMax(v) == intMax() );
+    }
+  }else{
+    SECTION("int is not 16 or 32 bit")
+    {
+      REQUIRE(false);
+    }
+  }
+}
+
+TEST_CASE("minBetweenSizeTypeValueAndIntMax_short_int")
+{
+  SECTION("0 is less than int max")
+  {
+    const short int v = 0;
+
+    CHECK( minBetweenSizeTypeValueAndIntMax(v) == 0 );
+  }
+
+  SECTION("25 is less than int max")
+  {
+    const short int v = 25;
+
+    CHECK( minBetweenSizeTypeValueAndIntMax(v) == 25 );
+  }
+
+  SECTION("-30 is less than int max")
+  {
+    const short int v = -30;
+
+    CHECK( minBetweenSizeTypeValueAndIntMax(v) == -30 );
+  }
+
+  if constexpr(sizeof(int) == 4){
+
+    /*
+     * short int: −32768 to 32767
+     * (signed) int: −2,147,483,648 to 2,147,483,647
+     */
+
+    SECTION("short int max is less than int max")
+    {
+      const short int v = std::numeric_limits<short int>::max();
+
+      CHECK( minBetweenSizeTypeValueAndIntMax(v) == v );
+    }
+
+    SECTION("short int min is less than int max")
+    {
+      const short int v = std::numeric_limits<short int>::min();
+
+      CHECK( minBetweenSizeTypeValueAndIntMax(v) == v );
+    }
+
+  }else if constexpr(sizeof(int) == 2){
+
+    /*
+     * short int: −32768 to 32767
+     * (signed) int:  −32768 to 32767
+     */
+
+    SECTION("short int max is equal to int max")
+    {
+      const short int v = std::numeric_limits<short int>::max();
+
+      CHECK( minBetweenSizeTypeValueAndIntMax(v) == intMax() );
+    }
+
+    SECTION("short int min is less than int max")
+    {
+      const short int v = std::numeric_limits<short int>::min();
+
+      CHECK( minBetweenSizeTypeValueAndIntMax(v) == v );
+    }
+
+  }else{
+    SECTION("int is not 16 or 32 bit")
+    {
+      REQUIRE(false);
+    }
+  }
+}
+
+TEST_CASE("minBetweenSizeTypeValueAndIntMax_int64")
+{
+  SECTION("0 is less than int max")
+  {
+    const int64_t v = 0;
+
+    CHECK( minBetweenSizeTypeValueAndIntMax(v) == 0 );
+  }
+
+  SECTION("25 is less than int max")
+  {
+    const int64_t v = 25;
+
+    CHECK( minBetweenSizeTypeValueAndIntMax(v) == 25 );
+  }
+
+  SECTION("-30 is less than int max")
+  {
+    const int64_t v = -30;
+
+    CHECK( minBetweenSizeTypeValueAndIntMax(v) == -30 );
+  }
+
+  SECTION("int64_t max is greater than int max")
+  {
+    const int64_t v = std::numeric_limits<int64_t>::max();
+
+    CHECK( minBetweenSizeTypeValueAndIntMax(v) == intMax() );
+  }
+
+  SECTION("int64_t min is less than int max - the result is int min")
+  {
+    const int64_t v = std::numeric_limits<int64_t>::min();
+
+    CHECK( minBetweenSizeTypeValueAndIntMax(v) == std::numeric_limits<int>::min() );
+  }
+}
+
+TEST_CASE("minBetweenSizeTypeMaxAndIntMax")
+{
+  SECTION("int max is less than size_t max")
+  {
+    CHECK( minBetweenSizeTypeMaxAndIntMax<std::size_t>() == intMax() );
   }
 }

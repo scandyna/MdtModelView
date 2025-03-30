d556fffa629e9cec6972a7781afaf6a13c1325a4
Philippe Steinmann <philippe.steinmann@gmail.com>
2025-03-30 10:56:24 +0200

    Work on shared STL container adapter
    GL-1

--- libs/ItemModel/src/Mdt/ItemModel/SharedStlContiguousContainerAdapter.h
+++ libs/ItemModel/src/Mdt/ItemModel/SharedStlContiguousContainerAdapter.h
@@ -10,8 +10,12 @@
 #ifndef MDT_ITEM_MODEL_SHARED_STL_CONTIGUOUS_CONTAINER_ADAPTER_H
 #define MDT_ITEM_MODEL_SHARED_STL_CONTIGUOUS_CONTAINER_ADAPTER_H
 
+#include "Mdt/TypeTraits/StlContainerHelpers.h"
+#include "Mdt/ItemModel/StlHelpers.h"
+#include "Mdt/ItemModel/NumericLimits.h"
 #include <Mdt/Numeric/Limits.h>
 #include <Mdt/Numeric/BasicConversion.h>
+#include <type_traits>
 #include <memory>
 #include <cassert>
 
@@ -20,8 +24,8 @@ namespace Mdt{ namespace ItemModel{
   /*! \brief Adapter to use shared STL contiguous containers with Qt item models
    *
    * This adapter can be used the same way as StlContiguousContainerAdapter.
-   * The difference is that the container not owned by this adapter,
-   * but managed with a shared pointer.
+   * The difference is that the container is not owned by this adapter,
+   * but referenced with a shared pointer.
    *
    * Example of some container:
    * \code
@@ -160,26 +164,348 @@ namespace Mdt{ namespace ItemModel{
   {
    public:
 
-    /*! \brief Construct an empty container
+    /*! \brief STL size_type
      *
-     * \todo define what it does
+     * Will be FunctionMap::size_type
      *
-     * \todo Think about size() and at()
-     * size(): check null
-     * at(): precondition in range, impies not null
+     * size_type is mandatory
+     */
+    using size_type = typename FunctionMap::size_type;
+
+    /*! \brief STL difference_type
+     *
+     * Will be FunctionMap::difference_type if \a FunctionMap defines it,
+     * otherwise void.
+     */
+    using difference_type = Mdt::TypeTraits::member_difference_type_or_void<FunctionMap>;
+
+    /*! \brief STL const_reference
+     *
+     * Will be FunctionMap::const_reference
+     *
+     * const_reference is mandatory
+     */
+    using const_reference = typename FunctionMap::const_reference;
+
+    /*! \brief STL reference
+     *
+     * Will be FunctionMap::reference if \a FunctionMap defines it,
+     * otherwise void.
+     */
+    using reference = Mdt::TypeTraits::member_reference_or_void<FunctionMap>;
+
+    /*! \brief STL const_iterator
+     *
+     * Will be FunctionMap::const_iterator if \a FunctionMap defines it,
+     * otherwise void*.
+     */
+    using const_iterator = Mdt::TypeTraits::member_const_iterator_or_void_pointer<FunctionMap>;
+
+    /*! \brief Construct an empty adapter
+     *
+     * An empty adapter does not reference any container,
+     * it holds a null pointer.
+     *
+     * \sa setContainer()
      */
     explicit
     SharedStlContiguousContainerAdapter() noexcept = default;
 
-    /*! \brief Get the count of elements
+    /*! \brief Set the container
+     *
+     * \pre \a container must be a valid pointer
      */
-    int size() const noexcept
+    void setContainer(std::shared_ptr<Container> container)
+    {
+      assert(container != nullptr);
+
+      mContainer = container;
+    }
+
+    /*! \brief Get the maximum allowed count of rows
+     *
+     * If this adaper does not reference a container,
+     * this method returns 0.
+     *
+     * If the function map provides a function to get the container's maximum allowed size,
+     * it will be used.
+     *
+     * This function must be of this form:
+     * \code
+     * static
+     * size_type maxSize(const Container & container);
+     * \endcode
+     *
+     * Returns the minimum value between:
+     * - the largest possible value for int
+     * - the largest possible value for size_type
+     * - FunctionMap::maxSize() if defined
+     */
+    int maxRowCount() const
+    {
+      if(!mContainer){
+        return 0;
+      }
+      if constexpr( Mdt::TypeTraits::has_member_maxSize_container<FunctionMap, Container>() ){
+        assert(mContainer != nullptr);
+        return minBetweenSizeTypeValueAndIntMax( FunctionMap::maxSize(*mContainer) );
+      }
+      return minBetweenSizeTypeMaxAndIntMax<size_type>();
+    }
+
+    /*! \brief Get the count of rows for the model
+     *
+     * Returns the size of the container if this adapter references one,
+     * otherwise 0.
+     *
+     * \pre The current size of the container must be convertible to int.
+     * The result must also be >= 0 and <= maxRowCount()
+     *
+     * \sa setContainer()
+     */
+    int rowCount() const
     {
       if(mContainer){
+        assert( Mdt::Numeric::int_canHoldValueOf_T( FunctionMap::size(*mContainer) ) );
+
+        const int count = Mdt::Numeric::int_from_T( FunctionMap::size(*mContainer) );
+        assert( count >= 0 );
+        assert( count <= maxRowCount() );
+
+        return count;
       }
       return 0;
     }
 
+    /*! \brief Check if given \a count rows can be added
+     *
+     * If rowCount() + \a count can't be represented by int,
+     * returns false.
+     *
+     * If rowCount() + \a count exceeds maxRowCount(),
+     * returns false.
+     *
+     * This also implies that rowCount() + \a count can be represented
+     * by size_type .
+     *
+     * \pre \a count must be >= 1
+     */
+    bool canAddCountRows(int count) const
+    {
+      assert( count >= 1 );
+
+      if( !Mdt::Numeric::canAdd(rowCount(), count) ){
+        return false;
+      }
+
+      return (rowCount() + count) <= maxRowCount();
+    }
+
+    /*! \brief Get the element at given row
+     *
+     * \pre \a row must be in range of the container ( 0 >= \a row < rowCount() )
+     * This also implies that a container has been set.
+     * \sa setContainer()
+     * \sa rowCount()
+     */
+    const_reference atRow(int row) const
+    {
+      assert( row >= 0 );
+      assert( row < rowCount() );
+      assert(mContainer != nullptr);
+
+      return FunctionMap::atIndex( *mContainer, indexFromRow(row) );
+    }
+
+    /*! \brief Access the element at given row for mutation
+     *
+     * \pre the reference type must be valid.
+     * \pre \a row must be in range ( 0 >= \a row < rowCount() )
+     * This also implies that a container has been set.
+     * \sa setContainer()
+     * \sa rowCount()
+     */
+    reference atRowMutable(int row) noexcept
+    {
+      static_assert( !std::is_void_v<reference>, "call SharedStlContiguousContainerAdapter::atRowMutable() requires FunctionMap::reference to be defined" );
+      assert( row >= 0 );
+      assert( row < rowCount() );
+      assert(mContainer != nullptr);
+
+      return FunctionMap::atIndexMutable( *mContainer, indexFromRow(row) );
+    }
+
+    /*! \brief Inserts count rows into the container before the given row
+     *
+     * To use this method, the function map must have an insert function of this form:
+     * \code
+     * static
+     * void insert(Container & container, const_iterator pos, size_type count, const_reference value);
+     * \endcode
+     *
+     * To define pos, the function map must also have a const-qualified cbegin:
+     * \code
+     * static
+     * const_iterator cbegin(const Container & container) noexcept;
+     * \endcode
+     *
+     * \pre FunctionMap::difference_type must be defined
+     * \pre FunctionMap::const_iterator must be defined
+     * \pre \a row must be >= 0
+     * \pre \a row must be <= rowCount()
+     * \pre \a count must be >= 1
+     * \pre it must be possible to add \a count rows
+     * This also implies that a container has been set.
+     * \sa setContainer()
+     * \sa canAddCountRows()
+     */
+    void insertRows(int row, int count, const_reference value)
+    {
+      static_assert( !std::is_void_v<difference_type>, "call SharedStlContiguousContainerAdapter::insertRows() requires FunctionMap::difference_type to be defined" );
+      static_assert( !Mdt::TypeTraits::is_void_or_void_pointer<const_iterator>(),
+                     "call SharedStlContiguousContainerAdapter::insertRows() requires FunctionMap::const_iterator to be defined" );
+      assert( row >= 0 );
+      assert( row <= rowCount() );
+      assert( count >= 1 );
+      assert( canAddCountRows(count) );
+      assert( mContainer != nullptr );
+
+      insertToStlContainer<Container, FunctionMap>(*mContainer, row, count, value);
+    }
+
+    /*! \brief Append an element
+     *
+     * To use this method, the function map must have a push_back function of this form:
+     * \code
+     * static
+     * void push_back(Container & container, const_reference value);
+     * \endcode
+     *
+     * \pre it must be possible to add 1 row
+     * This also implies that a container has been set.
+     * \sa setContainer()
+     * \sa canAddCountRows()
+     */
+    void appendRow(const_reference value)
+    {
+      assert( canAddCountRows(1) );
+      assert( mContainer != nullptr );
+
+      FunctionMap::push_back(*mContainer, value);
+    }
+
+    /*! \brief Removes count rows starting with the given row
+     *
+     * To use this method, the function map must have an erase function of this form:
+     * \code
+     * static
+     * void erase(Container & container, const_iterator first, const_iterator last);
+     * \endcode
+     *
+     * To define first and last, the function map must also have a const-qualified cbegin:
+     * \code
+     * static
+     * const_iterator cbegin(const Container & container) noexcept;
+     * \endcode
+     *
+     * \pre FunctionMap::difference_type must be defined
+     * \pre FunctionMap::const_iterator must be defined
+     * \pre \a row must be >= 0
+     * \pre \a count must be >= 1
+     * \pre ( \a row + \a count ) must be <= rowCount()
+     * This also implies that a container has been set.
+     * \sa setContainer()
+     * \sa rowCount()
+     */
+    void removeRows(int row, int count)
+    {
+      static_assert( !std::is_void_v<difference_type>, "call SharedStlContiguousContainerAdapter::removeRows() requires FunctionMap::difference_type to be defined" );
+      static_assert( !Mdt::TypeTraits::is_void_or_void_pointer<const_iterator>(),
+                     "call SharedStlContiguousContainerAdapter::removeRows() requires FunctionMap::const_iterator to be defined" );
+      assert( row >= 0 );
+      assert( count >= 1 );
+      assert( Mdt::Numeric::canAdd(row, count) );
+      assert( (row + count) > 0 );
+      assert( (row + count) <= rowCount() );
+
+      removeFromStlContainer<Container, FunctionMap>(*mContainer, row, count);
+    }
+
+    /*! \brief Get the size_type index from given row
+     *
+     * \pre \a row must be in range of the container ( 0 >= \a row < rowCount() )
+     * This also implies that a container has been set.
+     * \sa setContainer()
+     * \sa rowCount()
+     */
+    size_type indexFromRow(int row) const
+    {
+      assert( row >= 0 );
+      assert( row < rowCount() );
+      assert( mContainer != nullptr );
+
+      /*
+       * row is in the range of the container,
+       * it is also in the range of size_type
+       */
+      return static_cast<size_type>(row);
+    }
+
+    /*! \brief Get the row from given size_type index
+     *
+     * \pre \a index must be convertible to int
+     * \pre \a index must be in range of the container ( 0 >= \a index < container's size )
+     * \pre This adapter must reference a container
+     * \sa setContainer()
+     */
+    int rowFromIndex(size_type index) const
+    {
+      assert( Mdt::Numeric::int_canHoldValueOf_T(index) );
+      assert( index >= 0 );
+      assert( mContainer != nullptr );
+      assert( index < FunctionMap::size(*mContainer) );
+
+      return Mdt::Numeric::int_from_T(index);
+    }
+
+    /*! \brief Access the container
+     *
+     * \pre This adapter must reference a container
+     * \sa setContainer()
+     * \sa containerMutablePtr()
+     */
+    const Container & container() const noexcept
+    {
+      assert(mContainer != nullptr);
+
+      return *mContainer;
+    }
+
+    /*! \brief Access the container for mutation
+     *
+     * \pre This adapter must reference a container
+     * \sa setContainer()
+     * \sa containerMutablePtr()
+     */
+    Container & containerMutable() noexcept
+    {
+      assert(mContainer != nullptr);
+
+      return *mContainer;
+    }
+
+    /*! \brief Access the container for mutation
+     *
+     * Returns the pointer to the container.
+     *
+     * Returns a nullptr if this adapter references no container.
+     *
+     * \sa setContainer()
+     */
+    std::shared_ptr<Container> containerMutablePtr() noexcept
+    {
+      return mContainer;
+    }
 
    private:
 
--- libs/ItemModel/tests/src/SharedStlContiguousContainerAdapterTest.cpp
+++ libs/ItemModel/tests/src/SharedStlContiguousContainerAdapterTest.cpp
@@ -10,5 +10,233 @@
 #include "Mdt/ItemModel/SharedStlContiguousContainerAdapter.h"
 #include "catch2/catch.hpp"
 #include "Catch2QString.h"
+#include "Item.h"
+#include "DefaultConstructibleOnlyList.h"
+#include "CopyConstructibleOnlyList.h"
+#include "MoveConstructibleOnlyList.h"
+#include "DefaultConstructibleOnlyListTableModelAdapterFunctionMap.h"
+#include "CopyConstructibleOnlyListTableModelAdapterFunctionMap.h"
+#include "MoveConstructibleOnlyListTableModelAdapterFunctionMap.h"
+#include "ReadOnlyList.h"
+#include "ReadOnlyListTableModelAdapterFunctionMap.h"
+#include "MutableListRawData.h"
+#include "MutableListRawDataTableModelAdapterFunctionMap.h"
+#include "MutableList.h"
+#include "MutableListTableModelAdapterFunctionMap.h"
+#include "ListWithInsert.h"
+#include "ListWithInsertTableModelAdapterFunctionMap.h"
+#include "ListWithInsertAndLimit.h"
+#include "ListWithInsertAndLimitTableModelAdapterFunctionMap.h"
+#include "ListWithAppend.h"
+#include "ListWithAppendTableModelAdapterFunctionMap.h"
+#include "ListWithErase.h"
+#include "ListWithEraseTableModelAdapterFunctionMap.h"
 
 using namespace Mdt::ItemModel;
+
+
+using DefaultConstructibleOnlyListAdapted = SharedStlContiguousContainerAdapter<DefaultConstructibleOnlyList, DefaultConstructibleOnlyListTableModelAdapterFunctionMap>;
+using CopyConstructibleOnlyListAdapted = SharedStlContiguousContainerAdapter<CopyConstructibleOnlyList, CopyConstructibleOnlyListTableModelAdapterFunctionMap>;
+using MoveConstructibleOnlyListAdapted = SharedStlContiguousContainerAdapter<MoveConstructibleOnlyList, MoveConstructibleOnlyListTableModelAdapterFunctionMap>;
+
+using ReadOnlyListAdapted = SharedStlContiguousContainerAdapter<ReadOnlyList, ReadOnlyListTableModelAdapterFunctionMap>;
+
+using MutableListRawDataAdapted = SharedStlContiguousContainerAdapter<MutableListRawData, MutableListRawDataTableModelAdapterFunctionMap>;
+using MutableListAdapted = SharedStlContiguousContainerAdapter<MutableList, MutableListTableModelAdapterFunctionMap>;
+
+using ListWithInsertAdapted = SharedStlContiguousContainerAdapter<ListWithInsert, ListWithInsertTableModelAdapterFunctionMap>;
+using ListWithAppendAdapted = SharedStlContiguousContainerAdapter<ListWithAppend, ListWithAppendTableModelAdapterFunctionMap>;
+using ListWithInsertAndLimitAdapted = SharedStlContiguousContainerAdapter<ListWithInsertAndLimit, ListWithInsertAndLimitTableModelAdapterFunctionMap>;
+
+using ListWithEraseAdapted = SharedStlContiguousContainerAdapter<ListWithErase, ListWithEraseTableModelAdapterFunctionMap>;
+
+
+TEMPLATE_TEST_CASE("default_constructed", "", DefaultConstructibleOnlyListAdapted, CopyConstructibleOnlyListAdapted, MoveConstructibleOnlyListAdapted, ReadOnlyListAdapted)
+{
+  TestType list;
+
+  CHECK( list.rowCount() == 0 );
+}
+
+
+TEST_CASE("setContainer")
+{
+  ReadOnlyListAdapted adapter;
+
+  CHECK( adapter.rowCount() == 0 );
+
+  auto list = std::make_shared<ReadOnlyList>( ReadOnlyList::fromItemList({{1,"A"}}) );
+
+  adapter.setContainer(list);
+
+  CHECK( adapter.rowCount() == 1 );
+}
+
+TEST_CASE("maxRowCount_No_maxSize")
+{
+  ListWithInsertAdapted adapter;
+
+  CHECK( adapter.maxRowCount() == 0 );
+
+  auto list = std::make_shared<ListWithInsert>();
+
+  adapter.setContainer(list);
+
+  /*
+   * We use std::vector in the test containers,
+   * size_type is unsigned and at least unsigned int
+   * (but probably an unsigned 64 bit int).
+   */
+  CHECK( adapter.maxRowCount() == intMax() );
+}
+
+TEST_CASE("maxRowCount_With_maxSize")
+{
+  ListWithInsertAndLimitAdapted adapter;
+
+  CHECK( adapter.maxRowCount() == 0 );
+
+  auto list = std::make_shared<ListWithInsertAndLimit>();
+  list->setMaximumElementCount(25);
+
+  adapter.setContainer(list);
+
+  CHECK( adapter.maxRowCount() == 25 );
+}
+
+TEST_CASE("canAddCountRows")
+{
+  ListWithInsertAndLimitAdapted adapter;
+  auto list = std::make_shared<ListWithInsertAndLimit>();
+  list->setMaximumElementCount(5);
+  adapter.setContainer(list);
+  REQUIRE( adapter.maxRowCount() == 5 );
+  REQUIRE( adapter.rowCount() == 0 );
+
+  SECTION("empty list")
+  {
+    CHECK( adapter.canAddCountRows(1) );
+    CHECK( adapter.canAddCountRows(2) );
+    CHECK( adapter.canAddCountRows(4) );
+    CHECK( adapter.canAddCountRows(5) );
+    CHECK( !adapter.canAddCountRows(6) );
+    CHECK( !adapter.canAddCountRows(7) );
+    CHECK( !adapter.canAddCountRows( intMax() ) );
+  }
+
+  SECTION("adapter with 2 elements")
+  {
+    adapter.insertRows( 0, 2, Item() );
+    REQUIRE( adapter.rowCount() == 2 );
+
+    CHECK( adapter.canAddCountRows(1) );
+    CHECK( adapter.canAddCountRows(2) );
+    CHECK( adapter.canAddCountRows(3) );
+    CHECK( !adapter.canAddCountRows(4) );
+    CHECK( !adapter.canAddCountRows(5) );
+    CHECK( !adapter.canAddCountRows( intMax() ) );
+  }
+}
+
+TEST_CASE("indexFromRow")
+{
+  ReadOnlyListAdapted adapter;
+  auto list = std::make_shared<ReadOnlyList>( ReadOnlyList::fromItemList({{1,"A"}}) );
+  adapter.setContainer(list);
+  REQUIRE( adapter.rowCount() == 1 );
+
+  CHECK( adapter.indexFromRow(0) == 0 );
+}
+
+TEST_CASE("rowFromIndex")
+{
+  ReadOnlyListAdapted adapter;
+  auto list = std::make_shared<ReadOnlyList>( ReadOnlyList::fromItemList({{1,"A"},{2,"B"}}) );
+  adapter.setContainer(list);
+  REQUIRE( adapter.rowCount() == 2 );
+
+  CHECK( adapter.rowFromIndex(0) == 0 );
+  CHECK( adapter.rowFromIndex(1) == 1 );
+}
+
+TEST_CASE("ReadOnly_example")
+{
+  ReadOnlyListAdapted adapter;
+  auto list = std::make_shared<ReadOnlyList>( ReadOnlyList::fromItemList({{1,"A"}}) );
+  adapter.setContainer(list);
+
+  CHECK( adapter.rowCount() == 1 );
+  CHECK( adapter.atRow(0).id == 1 );
+}
+
+TEST_CASE("MutableListRawData_example")
+{
+  MutableListRawDataAdapted adapter;
+  auto list = std::make_shared<MutableListRawData>( MutableListRawData::fromItemList({{1,"A"}}) );
+  adapter.setContainer(list);
+  REQUIRE( adapter.rowCount() == 1 );
+  REQUIRE( adapter.atRow(0).name == "A" );
+
+  adapter.atRowMutable(0).name = "B";
+
+  CHECK( adapter.atRow(0).name == "B" );
+}
+
+TEST_CASE("MutableList_DomainMethods_example")
+{
+  MutableListAdapted adapter;
+  auto list = std::make_shared<MutableList>( MutableList::fromItemList({{1,"A"}}) );
+  adapter.setContainer(list);
+  REQUIRE( adapter.rowCount() == 1 );
+  REQUIRE( adapter.atRow(0).name == "A" );
+
+  const MutableList::size_type containerIndex = adapter.indexFromRow(0);
+  adapter.containerMutable().setNameAt(containerIndex, "C");
+
+  CHECK( adapter.atRow(0).name == "C" );
+
+  adapter.containerMutablePtr()->setNameAt(containerIndex, "D");
+
+  CHECK( adapter.atRow(0).name == "D" );
+
+  list->setNameAt(containerIndex, "E");
+
+  CHECK( adapter.atRow(0).name == "E" );
+}
+
+TEST_CASE("ListWithInsert_example")
+{
+  ListWithInsertAdapted adapter;
+  auto list = std::make_shared<ListWithInsert>();
+  adapter.setContainer(list);
+  REQUIRE( adapter.rowCount() == 0 );
+
+  adapter.insertRows( 0, 1, Item() );
+
+  CHECK( adapter.rowCount() == 1 );
+}
+
+TEST_CASE("ListWithAppend_example")
+{
+  ListWithAppendAdapted adapter;
+  auto list = std::make_shared<ListWithAppend>();
+  adapter.setContainer(list);
+  REQUIRE( adapter.rowCount() == 0 );
+
+  adapter.appendRow( Item() );
+
+  CHECK( adapter.rowCount() == 1 );
+}
+
+TEST_CASE("ListWithErase_example")
+{
+  ListWithEraseAdapted adapter;
+  auto list = std::make_shared<ListWithErase>( ListWithErase::fromItemList({{1,"A"}}) );
+  adapter.setContainer(list);
+  REQUIRE( adapter.rowCount() == 1 );
+  REQUIRE( adapter.atRow(0).name == "A" );
+
+  adapter.removeRows(0, 1);
+
+  CHECK( adapter.rowCount() == 0 );
+}
